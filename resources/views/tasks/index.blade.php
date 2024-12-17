0ed04cf9122e05486753e51f3fd6da1a3f558f65
Danila Egorov <ya.ul2010@yandex.ru>
2024-12-17 12:31:36 +0400

    add user role to a registered user

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -16,45 +16,46 @@
                     <form method="GET" action="{{ route('tasks.index') }}">
 
                         <select name="priority" id="status" class="dropdown">
-                            <option selected value="" class="btn-secondary dropdown-toggle">Select Priority</option>
-                            @foreach ($priorities as $priority)
-                            <option class="dropdown-item" value="{{ $priority }}"
-                                {{ request('priority') === $priority ? 'selected' : '' }}>{{ $priority }}
+                            <option selected value="" class="btn-secondary dropdown-toggle">Select Priority
                             </option>
+                            @foreach ($priorities as $priority)
+                                <option class="dropdown-item" value="{{ $priority }}"
+                                    {{ request('priority') === $priority ? 'selected' : '' }}>{{ $priority }}
+                                </option>
                             @endforeach
                         </select>
 
                         <select name="status" id="status" class="dropdown">
                             <option selected value="" class="btn-secondary dropdown-toggle">Select Status</option>
                             @foreach ($statuses as $status)
-                            <option class="dropdown-item" value="{{ $status }}"
-                                {{ request('status') === $status ? 'selected' : '' }}>{{ $status }}</option>
+                                <option class="dropdown-item" value="{{ $status }}"
+                                    {{ request('status') === $status ? 'selected' : '' }}>{{ $status }}</option>
                             @endforeach
                         </select>
                         <button type="submit" class="btn btn-dark">Select Filters</button>
                         <input type="hidden" name="sort" value="{{ request('sort') }}">
                         @if (request('status') || request('priority'))
-                        <a href="{{ route('tasks.index') }}" class="ml-3 text-red-500">Reset filters</a>
+                            <a href="{{ route('tasks.index') }}" class="ml-3 text-red-500">Reset filters</a>
                         @endif
                     </form>
                 </div>
                 <div class="inline-block float-right">
-                    <form method="GET" action="{{route('tasks.index')}}">
+                    <form method="GET" action="{{ route('tasks.index') }}">
                         <select name="sort">
-                            <option value="status" @if($sort==='status' ) selected @endif>Status</option>
-                            <option value="end_date" @if($sort==='end_date' ) selected @endif>Date</option>
-                            <option value="priority" @if($sort==='priority' ) selected @endif>Priority</option>
+                            <option value="status" @if ($sort === 'status') selected @endif>Status</option>
+                            <option value="end_date" @if ($sort === 'end_date') selected @endif>Date</option>
+                            <option value="priority" @if ($sort === 'priority') selected @endif>Priority</option>
                         </select>
-                        @foreach(request()->except('sort') as $key => $value)
-                        <input type="hidden" name="{{ $key }}" value="{{ $value }}">
+                        @foreach (request()->except('sort') as $key => $value)
+                            <input type="hidden" name="{{ $key }}" value="{{ $value }}">
                         @endforeach
                         <button type="submit">Sort</button>
                     </form>
                 </div>
                 @foreach ($tasks as $task)
-                <div class="mt-3">
-                    <x-task-card :task="$task" />
-                </div>
+                    <div class="mt-3">
+                        <x-task-card :task="$task" />
+                    </div>
                 @endforeach
                 {{ $tasks->links() }}
             </div>
