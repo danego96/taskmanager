49716ae0b543c647c20eeb538ae43552a323e92a
Danila Egorov <ya.ul2010@yandex.ru>
2024-12-17 11:15:54 +0400

    add roles into database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,13 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-        User::factory()->create([
+       $user = User::factory(10)->create();
+        $admin = User::factory()->create([
             'name' => 'Dan',
             'email' => 'dan@dan',
             'password' => '12345678',
         ]);
 
         Task::factory(12)->create();
+
+        $roleAdmin = Role::create(['name' => 'admin']);
+        $roleUser = Role::create(['name' => 'user']);
+
+        $admin -> assignRole($roleAdmin);
+        User::where('id', '!=', $admin->id)->get()->each(function ($user) use ($roleUser) {
+            $user->assignRole($roleUser);
+        });
     }
 }
