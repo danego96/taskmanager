73a6dbebf73ef3b8896975875db58fa04d71865c
Dan Egorov <ya.ul2010@yandex.ru>
2024-12-18 17:41:00 +0400

    fix task cards width

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,7 +8,7 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="py-12">
             <div
-                class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                class="p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                 @foreach ($tasks as $task)
                     <div class="mt-3">
                         <x-task-card :task="$task" />
