aebb7afe7832fbb5345eabfd691c7b111c166f8f
Danila Egorov <ya.ul2010@yandex.ru>
2024-12-20 12:24:00 +0400

    update user.table layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,26 +9,46 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="mt-3">
-              
-                <table class="table">
-                    <thead>
-                      <tr>
-                        <th scope="col">User ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Details</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($users as $user)
-                      <tr>
-                        <th scope="row">{{$user->id}} @if($user->hasRole('admin')) <span class="text-red-500">A</span> @endif</th>
-                        <td>{{$user->name}}</td>
-                        <td><a href="{{route('users.show', $user->id)}}">View Profile</a></td>
-                      </tr>
-                      @endforeach
-                    </tbody>
-                  </table>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    User ID
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Details
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr
+                                    class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {{ $user->id }} @if ($user->hasRole('admin'))
+                                            <span class="text-red-500">A
+                                        @endif
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {{ $user->name }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        <a href="{{ route('users.show', $user->id) }}">View Profile</a>
+                                    </td>
+                                </tr>
+                        </tbody>
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+            <div class="mt-2">
                 {{ $users->links() }}
             </div>
         </div>
+    </div>
 </x-app-layout>
